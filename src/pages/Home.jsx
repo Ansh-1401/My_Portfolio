fedf00a79bf0c8a358feaaeab7ed99064f0bf324
Ansh Srivastava <srivastavaansh1408@gmail.com>
2026-06-24 17:20:39 +0530

    Updated portfolio about section and education details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,7 +92,7 @@ const Home = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1xNgqJN2lhznD-1KN-5raWXXkcvrQoryL/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1hHOJB_XZjiCJ8oS7cUf0wl1aglqfWZkG/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-3 rounded-lg font-semibold
